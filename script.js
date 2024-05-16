fb109cf8d41fd6cceb06056d1f716ce25155845e
Juliana Andrade <julliana.andrade08@gmail.com>
2024-05-16 15:46:48 -0300

    :sparkles: feat: Add funções de alterar texto da pág

--- script.js
+++ script.js
@@ -6,20 +6,43 @@ const displayTempo = document.getElementById('timer')
 const banner = document.querySelector('.app__image')
 const tituloAlterna = document.querySelector('.app__title')
 const inicioPauseBtn = document.getElementById('start-pause')
+const botoes = document.querySelectorAll('.app__card-button')
 
 focoBtn.addEventListener('click', () => {
   alterarContexto('foco')
+  focoBtn.classList.add('active')
 })
 
 curtoBtn.addEventListener('click', () => {
   alterarContexto('descanso-curto')
+  curtoBtn.classList.add('active')
 })
 
 longoBtn.addEventListener('click', () => {
   alterarContexto('descanso-longo')
+  longoBtn.classList.add('active')
 })
 
 function alterarContexto(contexto) {
+  botoes.forEach(function (contexto){
+    contexto.classList.remove('active')
+  })
   hmtl.setAttribute('data-contexto', contexto)
-  banner.setAttribute('src',`/imagens/${contexto}.png`)
+  banner.setAttribute('src', `/imagens/${contexto}.png`)
+
+  switch (contexto) {
+    case 'foco':
+      tituloAlterna.innerHTML = `Otimize sua produtividade,<br>
+        <strong class="app__title-strong">mergulhe no que importa.</strong>`
+      break;
+    case 'descanso-curto':
+      tituloAlterna.innerHTML = `Que tal dar uma respirada? <br>
+        <strong class="app__title-strong"> Faça uma pausa curta!</strong>`
+      break;    
+    case 'descanso-longo':
+      tituloAlterna.innerHTML = `Hora de voltar à superfície. <br>
+        <strong class="app__title-strong"> Faça uma pausa longa.</strong>`
+    default:
+      break;
+  }
 }
